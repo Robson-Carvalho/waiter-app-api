554898480e8d71fd096890b68666f7bcefed4c10
Robson-Carvalho <robsoncarvalho.tech@gmail.com>
2023-03-22 23:30:05 -0300

    :recycle: refactored code

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,22 @@ import express from "express";
 import mongoose from "mongoose";
 require("dotenv").config();
 
-const app = express();
-
 mongoose
   .connect(
     `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.yr7ugsu.mongodb.net/?retryWrites=true&w=majority`
   )
-  .then(() => console.log("✅ Server connected to mongoDB Atlas successfully"))
-  .catch(() => console.log("❌ Error connecting to mongoDB Atlas"));
+  .then(() => {
+    const app = express();
+    const port = 3001;
 
-const port = 3001;
-app.listen(port, () => {
-  console.log(`🚀 Server is running on http://localhost:${port}`);
-});
+    app.listen(port, () => {
+      console.log(
+        `✅ Server connected to mongoDB Atlas successfully \n🚀 Server is running on http://localhost:${port}`
+      );
+    });
+  })
+  .catch(() =>
+    console.log(
+      "❌ Error connecting to mongoDB Atlas\n❌ Server is not running"
+    )
+  );
