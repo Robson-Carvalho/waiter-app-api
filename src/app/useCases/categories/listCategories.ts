e2e80e30994a74afe0dd762ed5847329eee97efa
Robson-Carvalho <robsoncarvalho.tech@gmail.com>
2023-03-23 14:53:41 -0300

    :recycle: refactor: error handling in list categories use case

--- src/app/useCases/categories/listCategories.ts
+++ src/app/useCases/categories/listCategories.ts
@@ -3,6 +3,11 @@ import { Request, Response } from "express";
 import { Category } from "../../models/Category";
 
 export const listCategories = async (req: Request, res: Response) => {
-  const categories = await Category.find();
-  res.json(categories);
+  try {
+    const categories = await Category.find();
+    res.json(categories);
+  } catch (error) {
+    res.sendStatus(500);
+    console.log(error);
+  }
 };
