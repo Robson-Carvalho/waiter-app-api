4dfd166e94cb077493de69aa8b76d1d3261fea9f
Robson-Carvalho <robsoncarvalho.tech@gmail.com>
2023-03-22 23:18:30 -0300

    :sparkles: server connected to mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 import express from "express";
+import mongoose from "mongoose";
 require("dotenv").config();
 
 const app = express();
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.yr7ugsu.mongodb.net/?retryWrites=true&w=majority`
+  )
+  .then(() => console.log("✅ Server connected to mongoDB Atlas successfully"))
+  .catch(() => console.log("❌ Error connecting to mongoDB Atlas"));
 
 const port = 3001;
 app.listen(port, () => {
