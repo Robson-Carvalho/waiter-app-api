6dd7159be03e013d3b207fd013ab8bb145ff7444
Robson-Carvalho <robsoncarvalho.tech@gmail.com>
2023-04-05 16:06:48 -0300

    :bug: error in accessing cors policy fixed

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,14 @@ mongoose
   )
   .then(() => {
     const app = express();
+
+    app.use((req, res, next) => {
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.setHeader("Access-Control-Allow-Methods", "*");
+      res.setHeader("Access-Control-Allow-Headers", "*");
+
+      next();
+    });
     app.use(express.static("uploads"));
     app.use(express.json());
     app.use(router);
